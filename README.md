51a704b2226fdd9c12c34a6d7f99c6a5852d3188
Shubham Tandon <standon@ncsu.edu>
2020-05-24 16:26:57 +0000

    Fixed the marker issue

---
+++ README.md
@@ -0,0 +1,22 @@
+# ROBOND Home Service Robot
+
+This project is part of the Udacity Robotics Nanodegree program. In this project, I used a world file developed earlier in the Nanodegree. Then a turtlebot was deployed in the environment by linking the world to it. 
+
+## Packages Used
+The following packages were instaled:
+```
+git clone https://github.com/ros-perception/slam_gmapping.git
+git clone https://github.com/turtlebot/turtlebot.git
+git clone https://github.com/turtlebot/turtlebot_interactions.git
+git clone https://github.com/turtlebot/turtlebot_simulator.git
+```
+
+## Shell Scripts
+Then the following shell scripts were created (kept in /src/shellscripts):
+**test_slam.sh**: To launch turtlebot in myworld and observe map in rviz. Also, the bot could be navigated with keyboard using teleop package.
+**test_navigation.sh**: To la
+
+
+<img src="images/course_code_structure.png" width="779" height="414" />
+
+
--- src/add_markers/src/add_markers.cpp
+++ src/add_markers/src/add_markers.cpp
@@ -3,7 +3,7 @@
 #include <visualization_msgs/Marker.h>
 
 float pickup[3]    = {2.0, 3.0, 1.0};
-float dropoff[3]   = {1.0,-4.0,-1.0};
+float dropoff[3]   = {1.0, -4.0, -1.0};
 float threshold[2] = {0.4, 0.4};
 
 bool atPickup = false;
@@ -11,20 +11,29 @@ bool finishPickup = false;
 bool atDropOff = false;
 bool finishDropOff = false;
 
+visualization_msgs::Marker marker;
+
 
 void odomCallback(const nav_msgs::Odometry::ConstPtr &msg) 
 {
-
-    float posX = msg->pose.pose.position.x;
-    float posY = msg->pose.pose.position.y;
+    
+    //Conversion from Map frame to odom
+    float posX =  msg->pose.pose.position.y;
+    float posY = -msg->pose.pose.position.x;
+    //ROS_INFO("Inside ODOM");
+    //ROS_INFO("x: %f, y: %f", posX, posY);
 
     if (std::abs(posX - pickup[0]) < threshold[0] && std::abs(posY - pickup[1]) < threshold[1]) 
     {
-        // pause 5s to simulate pickup
-        ros::Duration(5).sleep();
         // hide marker and set it to new coordinates
         atPickup = true;
+        // Set the color -- be sure to set alpha to something non-zero!
+        marker.color.r = 0.0f;
+        marker.color.g = 1.0f;
+        marker.color.b = 0.0f;
 		ROS_INFO("At pickup location...");
+	    // pause 5s to simulate pickup
+        ros::Duration(5).sleep();
     } 
     else
     {
@@ -54,48 +63,51 @@ int main( int argc, char** argv )
   // Set our initial shape type to be a cube
   uint32_t shape = visualization_msgs::Marker::CUBE;
 
-
-
-    visualization_msgs::Marker marker;
-    // Set the frame ID and timestamp.  See the TF tutorials for information on these.
-    marker.header.frame_id = "map";
-    marker.header.stamp = ros::Time::now();
-
-    // Set the namespace and id for this marker.  This serves to create a unique ID
-    // Any marker sent with the same namespace and id will overwrite the old one
-    marker.ns = "add_markers";
-    marker.id = 0;
-
-    // Set the marker type.  Initially this is CUBE
-    marker.type = shape;
-
-    // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
-    marker.action = visualization_msgs::Marker::ADD;
-
-    // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-    marker.pose.position.x = pickup[0];
-    marker.pose.position.y = pickup[1];
-    marker.pose.orientation.x = 0.0;
-    marker.pose.orientation.y = 0.0;
-    marker.pose.orientation.z = 0.0;
-    marker.pose.orientation.w = pickup[2];
-
-    // Set the scale of the marker -- 1x1x1 here means 1m on a side
-    marker.scale.x = 0.5;
-    marker.scale.y = 0.5;
-    marker.scale.z = 0.5;
-
-    // Set the color -- be sure to set alpha to something non-zero!
-    marker.color.r = 0.0f;
-    marker.color.g = 1.0f;
-    marker.color.b = 0.0f;
-    marker.color.a = 1.0;
-
-    marker.lifetime = ros::Duration();
-    marker_pub.publish(marker);
-    
    while (ros::ok())
    {
+
+
+        
+        // Set the frame ID and timestamp.  See the TF tutorials for information on these.
+        marker.header.frame_id = "map";
+        marker.header.stamp = ros::Time::now();
+
+        // Set the namespace and id for this marker.  This serves to create a unique ID
+        // Any marker sent with the same namespace and id will overwrite the old one
+        marker.ns = "basic_shapes";
+        marker.id = 0;
+
+        // Set the marker type.  Initially this is CUBE
+        marker.type = shape;
+
+        // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
+        marker.action = visualization_msgs::Marker::ADD;
+
+        // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
+        
+        if (!atDropOff && !finishDropOff)
+        {
+            marker.pose.position.x = pickup[0];
+            marker.pose.position.y = pickup[1];
+            marker.pose.orientation.x = 0.0;
+            marker.pose.orientation.y = 0.0;
+            marker.pose.orientation.z = 0.0;
+            marker.pose.orientation.w = pickup[2];
+
+            // Set the scale of the marker -- 1x1x1 here means 1m on a side
+            marker.scale.x = 0.25;
+            marker.scale.y = 0.25;
+            marker.scale.z = 0.25;
+
+            // Set the color -- be sure to set alpha to something non-zero!
+            marker.color.r = 1.0f;
+            marker.color.g = 0.0f;
+            marker.color.b = 0.0f;
+            marker.color.a = 1.0;
+        }
+
+        marker.lifetime = ros::Duration();
+    
 
         // Publish the marker
         while (marker_pub.getNumSubscribers() < 1)
@@ -108,12 +120,15 @@ int main( int argc, char** argv )
           sleep(1);
         }
 
+        marker_pub.publish(marker);
 
+    
         while(!atPickup)
         {
             ros::spinOnce();
         }
-
+        
+        ROS_INFO("atPickUp: %d, finishPickup: %d", atPickup, finishPickup);
         if(atPickup && !finishPickup)
         {
             marker.action = visualization_msgs::Marker::DELETE;
@@ -133,6 +148,11 @@ int main( int argc, char** argv )
             marker.pose.position.x = dropoff[0];
             marker.pose.position.y = dropoff[1];
             marker.pose.orientation.w = dropoff[2];
+            
+            // Set the color -- be sure to set alpha to something non-zero!
+            marker.color.r = 0.0f;
+            marker.color.g = 0.0f;
+            marker.color.b = 1.0f;
             marker.action = visualization_msgs::Marker::ADD;
             marker_pub.publish(marker);
             ROS_INFO("Drop-off marker displayed");
